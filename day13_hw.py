c1035c79d01a19058d1fa66200ecda7740fb9094
arm-210-dev <arm212142@gmail.com>
2026-07-26 23:08:12 +0600

    Open Code

---
+++ day13_hw.py
@@ -0,0 +1,8 @@
+username = input("Enter your username:")
+password = input("Enter your password:")
+
+if username == "admin" and password == "admin123":
+  print("Login Successful.")
+else: 
+  print("Opps! Username or Password Wrong")
+  
---
+++ day13_hw2.py
@@ -0,0 +1,5 @@
+number = float(input("Enter your number:"))
+if number >=15 and number <=90 :
+  print("Number is between range.")
+else:
+  print("Number is outside the range.")
